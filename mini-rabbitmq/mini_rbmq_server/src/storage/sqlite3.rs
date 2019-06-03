e5654d01041ce3abab3ee040fb514d328d7906c4
MwlLj <731025894@qq.com>
2019-06-03 20:33:35 +0800

    新增获取接口

--- mini-rabbitmq/mini_rbmq_server/src/storage/sqlite3.rs
+++ mini-rabbitmq/mini_rbmq_server/src/storage/sqlite3.rs
@@ -29,7 +29,7 @@ impl CSqlite3 {
                 exchange_type varchar(64)
             );
             create table if not exists t_queue_info (
-                queue_name varchar(64) primary key,
+                queue_name varchar(64) primary key
             );
             create table if not exists t_bind_info (
                 exchange_name varchar(64),
@@ -45,13 +45,16 @@ impl CSqlite3 {
     }
 
     pub fn createExchange(&self, exchangeName: &str, exchangeType: &str) -> sqlite3::Result<()> {
-        let sql = format!(
-            "
-            insert into t_exchange_info values('{}', '{}');
-            "
-        , exchangeName, exchangeType);
-        if let Ok(ref conn) = self.connect {
-            conn.execute(sql)?
+        let count = self.getExchangeCount(exchangeName);
+        if count == 0 {
+            let sql = format!(
+                "
+                insert into t_exchange_info values('{}', '{}');
+                "
+            , exchangeName, exchangeType);
+            if let Ok(ref conn) = self.connect {
+                conn.execute(sql)?
+            }
         }
         Ok(())
     }
@@ -106,6 +109,47 @@ impl CSqlite3 {
             conn.execute(sql)?
         }
         Ok(())
+    }
+}
+
+impl CSqlite3 {
+    fn getExchangeCount(&self, exchangeName: &str) -> u32 {
+        let mut count = 0 as u32;
+        self.getSingle(
+            "
+            select count(0) from t_exchange_info where exchange_name = ?;
+            "
+        , &[sqlite3::Value::String(String::from(exchangeName))], &mut |v: &[sqlite3::Value]| {
+            if let Some(value) = v[0].as_integer() {
+                count = value as u32;
+            };
+        });
+        count
+    }
+}
+
+impl CSqlite3 {
+    fn getSingle<Func>(&self, sql: &str, params: &[sqlite3::Value], callback: &mut Func)
+        where Func: FnMut(&[sqlite3::Value]) {
+        let conn = match self.connect {
+            Ok(ref conn) => conn,
+            Err(_) => return,
+        };
+        let pre = match conn.prepare(sql) {
+            Ok(pre) => pre,
+            Err(_) => return,
+        };
+        let mut cursor = pre.cursor();
+        if let Err(_) = cursor.bind(params) {
+            return;
+        }
+        let next = match cursor.next() {
+            Ok(next) => next,
+            Err(_) => return,
+        };
+        if let Some(row) = next {
+            callback(row);
+        }
     }
 }
 
